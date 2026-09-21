f5a61e90aa96736e9300469d26b033f5d0bbfe39
valeria-ria-st <valeria_stolyarova@mail.ru>
2026-09-21 15:39:15 +0300

    Add files via upload

---
+++ integer2.py
@@ -0,0 +1,35 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "xpython",
+      "display_name": "Python 3.13 (XPython)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "ipython",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.13.1"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "58d653fd-c990-4e03-8e52-2706af1610d7",
+      "cell_type": "code",
+      "source": "M = int(input(\"Введите число\"))\ntonnes = M // 1000\nprint(tonnes)",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    }
+  ]
+}
