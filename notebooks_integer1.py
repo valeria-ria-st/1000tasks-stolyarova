169f2aa8dcd11a32f61a5784efde7790b7976de9
valeria-ria-st <valeria_stolyarova@mail.ru>
2026-09-21 15:36:11 +0300

    Add files via upload

---
+++ notebooks_integer1.py
@@ -0,0 +1,56 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "xpython",
+      "display_name": "Python 3.13 (XPython)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "ipython",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.13.1"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "612e6fe0-177e-4453-8045-623a519add4d",
+      "cell_type": "code",
+      "source": "L = int(input(\"Введите число\"))\nmetres = L // 100\nprint(metres)",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdin",
+          "text": "Введите число 45\n"
+        },
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "0\n"
+        }
+      ],
+      "execution_count": 1
+    },
+    {
+      "id": "4a75814a-72e5-4f6f-98ee-92b173d4f612",
+      "cell_type": "code",
+      "source": "",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": null
+    }
+  ]
+}
